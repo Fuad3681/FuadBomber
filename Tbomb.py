db1d14db222f0ff0a10fd911564da7ae5d32a670
Fuad3681 <106003986+Fuad3681@users.noreply.github.com>
2023-08-06 22:06:08 +0600

    Update Tbomb.py

--- Tbomb.py
+++ Tbomb.py
@@ -101,7 +101,7 @@ def logo():
     print("\033[94m│ \033[95mAuthor : ToxicNoob Inc.                \033[94m│".center(columns+15))
     print("│ \033[95mTool   : Unlimited SMS Bomber          \033[94m│".center(columns+9))
     print("│ \033[95mGitHub : https://github.com/Toxic-Noob \033[94m│".center(columns+9))
-    print("│ \033[95mCoder  : HunterSl4d3              \033[37mV4.1 \033[94m│".center(columns+15))
+    print("│ \033[95mCoder  : Fuad             \033[37mV4.1 \033[94m│".center(columns+15))
     print("\033[94m└────────────────────────────────────────┘".center(columns+5))
 
 
@@ -136,7 +136,7 @@ def check(sent):
         psb("\033[92m    [\033[37m*\033[92m] Target : \033[37m0" + main.number)
         psb("\033[92m    [\033[37m*\033[92m] From   : \033[37mToxicBomber\n")
         time.sleep(0.6)
-        print("\033[92m[\033[93m★\033[92m] Thanks For Using Our Tool \033[92m[\033[93m★\033[92m]".center(columns + 30))
+        print("\033[92m[\033[93m★\033[92m] Thanks For Using Our Tool by fuad\033[92m[\033[93m★\033[92m]".center(columns + 30))
         print("\033[37m")
         
         return True
